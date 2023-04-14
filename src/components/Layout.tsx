82736c511c7163a20aa0a1e91b4bffa7be166c37
Dmytro Shcherba <dscherba76@gmail.com>
2023-04-14 19:54:37 +0300

    add server auth to new-post page & minor style update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,12 @@
 import { PropsWithChildren } from 'react';
-import { Footer } from './footer';
-import { Header } from './header';
+import { Footer } from './Footer';
+import { Header } from './Header';
 
 export const Layout = ({ children }: PropsWithChildren) => {
     return (
         <>
             <Header />
-            <main>{children}</main>
+            <main style={{padding: '0 10px'}}>{children}</main>
             <Footer />
         </>
     );
