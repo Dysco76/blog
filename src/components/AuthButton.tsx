f8f82a43609e9aaab8fdcbd7ca26fe5c89ac55b4
Dmytro Shcherba <dscherba76@gmail.com>
2023-05-28 19:56:05 +0300

    add basic styles to auth button

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,32 +1,37 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
+import styled from 'styled-components';
 export const AuthButton = () => {
     const { data: session } = useSession();
     if (session) {
         return (
             <>
-                Signed in as {session?.user?.email} <br />
-                <button
+                Signed in as {session?.user?.email} 
+                <AuthButtonStyled
                     onClick={(e) => {
                         e.preventDefault();
                         signOut();
                     }}
                 >
                     Sign out
-                </button>
+                </AuthButtonStyled>
             </>
         );
     }
     return (
         <>
-            Not signed in <br />
-            <button
+            Not signed in 
+            <AuthButtonStyled
                 onClick={(e) => {
                     e.preventDefault();
                     signIn();
                 }}
             >
                 Sign in
-            </button>
+            </AuthButtonStyled>
         </>
     );
 };
+
+const AuthButtonStyled = styled.button`
+    margin-left: 10px;
+`;
